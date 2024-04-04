35dbbeba7e539aa5d533c1810e37344e0cdd9dea
LevaniMesxia23 <levaniko.mesxo.2004@gmail.com>
2024-04-04 21:15:03 +0400

    refactor App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,30 @@
+import Cards from "./components/Cards";
+import FormsHeader from "./components/FormsHeader";
+import FormsFooter from "./components/FormsFooter";
+import GlobalStyles from "./styles/GlobalStyles";
+import styled from "styled-components";
 
 function App() {
-
   return (
     <>
-      <div>
-        
-      </div>
+      <GlobalStyles />
+      <Container>
+        <ImagesBox></ImagesBox>
+        <FormDiv>
+          <NameNumber></NameNumber>
+          <ExpDate></ExpDate>
+        </FormDiv>
+      </Container>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
+
+const Container = styled.div``;
+
+const ImagesBox = styled.div``;
+
+const FormDiv = styled.div``;
+const NameNumber = styled.div``;
+const ExpDate = styled.div``;
--- src/components/FormsFooter.tsx
+++ src/components/FormsFooter.tsx
@@ -1,9 +1,9 @@
-import React from 'react'
+
 
 export default function FormsFooter() {
   return (
     <div>
-      
+
     </div>
   )
 }
