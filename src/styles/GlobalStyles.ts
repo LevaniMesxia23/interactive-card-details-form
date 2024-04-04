cd64320f90712b48d8d0aeafdaa4a840abcf2c56
LevaniMesxia23 <levaniko.mesxo.2004@gmail.com>
2024-04-04 21:20:40 +0400

    added font

--- src/styles/GlobalStyles.ts
+++ src/styles/GlobalStyles.ts
@@ -1,10 +1,12 @@
 import { createGlobalStyle } from "styled-components";
 
 const GlobalStyles = createGlobalStyle`
+@import url('https://fonts.googleapis.com/css2?family=Space+Grotesk:wght@300..700&family=Space+Mono:ital,wght@0,400;0,700;1,400;1,700&display=swap');
 *{
   margin: 0;
   padding: 0;
   box-sizing: border-box;
+  font-family: "Space Grotesk", sans-serif;
 }
 `
 export default GlobalStyles
